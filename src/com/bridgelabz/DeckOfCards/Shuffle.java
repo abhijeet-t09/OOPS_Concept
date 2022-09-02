ca9d8c9a19444155763fab90377677bf6f3c6ff5
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-09-02 11:24:01 +0530

    OOPS Concept: First Commit

---
+++ src/com/bridgelabz/DeckOfCards/Shuffle.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.DeckOfCards;
+
+public class Shuffle {
+    void shuffleCards(String[][] arr){
+        String temp;
+
+        for (int i = 0; i < 4; i++) {
+
+            for (int j = 0; j < 13; j++) {
+
+                int a = (int)(Math.random()*10)%4;
+                int b = (int)(Math.random()*10)%13;
+
+                temp = arr[i][j];
+                arr[i][j] = arr[a][b];
+                arr[a][b] = temp;
+            }
+        }
+    }
+}
